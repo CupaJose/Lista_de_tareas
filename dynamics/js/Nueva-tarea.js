0f005f9ad6932c7800c09670dd5e9aab09cb517b
Cupa José <cupa.sepulveda.jc@gmail.com>
2023-06-09 18:44:24 -0600

    Agregue opcion de marcar completada y eliminar incompletas

--- dynamics/js/Nueva-tarea.js
+++ dynamics/js/Nueva-tarea.js
@@ -3,6 +3,18 @@ const materiae= document.getElementById("inputGroupSelect01");
 const Tarea=document.getElementById("Tarea");
 const Agregar=document.getElementById("Agregar");
 const AreaTareas = document.getElementById("Area_tareas");
+const opciones = document.createElement("div");
+const tareasHacer = document.getElementById("tareas-hacer");
+const listo = document.createElement("button");
+listo.innerHTML+="Completa";
+listo.style.width="50%";
+listo.style.backgroundColor="#0000ff";
+const borrar = document.createElement("button");
+borrar.innerHTML+="Eliminar";
+borrar.style.width="50%";
+borrar.style.backgroundColor="#0000ff";
+// const arriba = document.createElement("button");
+// const abajo = document.createElement("button");
 var materias = []; //materias que se van agregando
 var tareas = []; //tareas que se van agregando
 var exis=false; //para comprobar si existe la materia o tarea
@@ -13,6 +25,9 @@ var i=0; //variable que me ayuda en la comprobacion de la existencia de tareas
 var loct =0; //localidades existentes en tareas
 var taream; //Variable para mostrar en la pagina la tarea
 var materiam; //variable para mostrar en la pagina la materia
+var Tareasfalt = 0;
+var tareascomplet=0;
+tareasHacer.innerHTML = "Tareas: Faltantes: "+Tareasfalt+" Hechas: "+tareascomplet;
 Agregar.addEventListener("click", ()=>{
     if (materiae.value=="Nueva")
     {
@@ -54,16 +69,19 @@ Agregar.addEventListener("click", ()=>{
         console.log(exis);
         if(exis==false)
         {
+        Tareasfalt++;
+        tareasHacer.innerHTML = "Tareas: Faltantes: "+Tareasfalt+" Hechas: "+tareascomplet;
         console.log(materias);
         console.log(materian.value);
         console.log(locu);
         console.log(materias[locu]);
         materiae.innerHTML +=  `<option value= ${materiam}> ${materiam}</option>`;
         console.log(Tarea.value);
-        AreaTareas.innerHTML += `<li class="list-group-item list-group-item-info">Tarea ${taream} de la materia ${materiam}</li>`;
+        AreaTareas.innerHTML += `<li class="list-group-item list-group-item-info">Tarea ${taream} de la materia ${materiam} </li>`;
+        AreaTareas.appendChild(opciones); 
+        opciones.appendChild(listo);
+        opciones.appendChild(borrar);
         }
-        // materias[locu-1] = materias[locu-1].toUpperCase();
-        // console.log(materias[locu-1]);
         materian.value="";
         Tarea.value="";
     }
@@ -75,6 +93,8 @@ Agregar.addEventListener("click", ()=>{
         }
         else
         {
+            Tareasfalt++;
+            tareasHacer.innerHTML = "Tareas: Faltantes: "+Tareasfalt+" Hechas: "+tareascomplet;
             materiam = materiae.value;
             taream = Tarea.value;
             materiae.value=materiae.value.toUpperCase();
@@ -107,6 +127,9 @@ Agregar.addEventListener("click", ()=>{
                 console.log(materias[locu]);
                 console.log(Tarea.value);
                 AreaTareas.innerHTML += `<li class="list-group-item list-group-item-info">Tarea ${taream} de la materia ${materiam}</li>`;
+                AreaTareas.appendChild(opciones); 
+                opciones.appendChild(listo);
+                opciones.appendChild(borrar);
             }
             // materias[locu-1] = materias[locu-1].toUpperCase();
             // console.log(materias[locu-1]);
@@ -114,6 +137,19 @@ Agregar.addEventListener("click", ()=>{
             Tarea.value="";
         }
     }
+    listo.addEventListener("click", ()=>{
+        if(Tareasfalt>0)
+        {
+            Tareasfalt--;
+            tareascomplet++;
+            tareasHacer.innerHTML = "Tareas: Faltantes: "+Tareasfalt+" Hechas: "+tareascomplet;
+        }
+        else
+            alert("No tiene ninguna tarea faltante");
+    });
+    // borrar.addEventListener("click",()=>{
+
+    // });
         
     // materiaNew = document.cookie = "materia=" + materias[locu-1] + "; max-age = 60*60";
 });
